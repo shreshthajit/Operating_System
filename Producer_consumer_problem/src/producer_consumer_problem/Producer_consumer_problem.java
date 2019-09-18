0c42ed707de22c77b09bbb5998d895c05a7c6f5a
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2019-09-19 02:24:56 +0600

    Add files via upload

---
+++ Producer_consumer_problem/src/producer_consumer_problem/Producer_consumer_problem.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package producer_consumer_problem;
+ 
+import java.util.LinkedList;
+
+public class Producer_consumer_problem {
+
+    
+    public static void main(String[] args) throws InterruptedException
+    {
+         final PC pc=new PC();
+         Thread t1=new Thread(new Runnable()
+         {
+         
+         
+         @Override
+                 public void run()
+                 {
+                     try
+                     {
+                         pc.produce();
+                     }catch(InterruptedException e)
+                             {
+                                  e.printStackTrace(); 
+                             }
+                 }
+         
+         
+                 });
+         
+         Thread t2=new Thread(new Runnable()
+         {
+         
+         @Override
+         public void run()
+         {
+             try
+             {
+                 pc.consume();
+             }catch(InterruptedException e)
+             {
+                 e.printStackTrace();
+             }
+         }
+         
+         
+         
+                 });
+         
+         t1.start();
+         t2.start();
+         
+         
+         
+         t1.join();
+         t2.join();
+         
+    }
+    
+    public static class PC
+
+            {
+                LinkedList<Integer>list=new LinkedList<>();
+                int capacity=2;
+                
+                public void produce() throws InterruptedException
+                {
+                    int value=0;
+                    while(true)
+                            {
+                                synchronized(this)
+                                {
+                                 while(list.size()==capacity)
+                                     wait();
+                                 
+                                    System.out.println("Producer produced" +value);
+                                    
+                                    
+                                    list.add(value++);
+                                    
+                                    notify();
+                                    Thread.sleep(1000);
+                                }
+                                
+                            }
+                }
+                
+                public void consume() throws InterruptedException
+                        {
+                        while(true)
+                        {
+                            synchronized(this)
+                                    {
+                                        while(list.size()==0)
+                                        
+                                        wait();
+                                        int val=list.removeFirst();
+                                        
+                                            System.out.println("consumer consumed" + val);
+                                            notify();
+                                            Thread.sleep(1000);
+                                      
+                                    }
+                        }
+                        }
+                        
+                
+            }
+}
